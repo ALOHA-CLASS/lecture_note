8beddfbfbe072d35bcb78e505d5d0fee7a9e0ed6
wwwalohacampus <53545071+wwwalohacampus@users.noreply.github.com>
2023-06-06 20:41:53 +0900

    Add files via upload

---
+++ 준비/R/code/hello.R
@@ -0,0 +1,20 @@
+print("Hello R~!")
+
+help()
+
+x <-10
+x <- c(1,2,3)
+x <- c(1:10)
+
+x <- seq(1,20,by=2)
+x <- seq(1,20)
+
+
+x <- c(1:18)
+y <- matrix( x, nrow=2, ncol=9, byrow=TRUE )
+array( y, dim=c(2,3,3) )
+
+
+a <- list( 1,2,3,'a','b','c' )
+
+
